f57a873cba17e617d1d121064b4f4b0b0d3f2c71
Sivanshu Jaiswal <shivanshu5416jaiswal@gmail.com>
2026-06-02 20:22:42 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import { useState } from 'react';
-import './App.css';
-import About from './components/About';
-import Alert from './components/Alert';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import About from "./components/About";
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
-  const [mode, setDarkMode] = useState('light')
-  const [alert, setAlert] = useState(null)
+  const funn = (data) => {
+    console.log("xxxx", data);
+  };
+  const [mode, setDarkMode] = useState("light");
+  const [alert, setAlert] = useState(null);
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
+      type: type,
+    });
     setTimeout(() => {
-      setAlert(null)
+      setAlert(null);
     }, 1500);
-  }
+  };
   const toggleMode = () => {
     if (mode == "light") {
-      setDarkMode("dark")
-      document.body.style.backgroundColor = "#042743"
-      showAlert("Dark Mode Enabled", "success")
-      document.title = "TextUtils-Dark Mode"
+      setDarkMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark Mode Enabled", "success");
+      document.title = "TextUtils-Dark Mode";
       // to show diifferent title continuoulsy
       // setInterval(() => {
       //   document.title = "Install dsf"
@@ -37,38 +35,41 @@ function App() {
       //   document.title = "Iddsf"
 
       // }, 1500);
+    } else {
+      setDarkMode("light");
+      document.body.style.backgroundColor = "white";
+      document.title = "TextUtils-Light Mode";
+      showAlert("Light Mode Enabled", "success");
     }
-    else {
-      setDarkMode("light")
-      document.body.style.backgroundColor = "white"
-      document.title = "TextUtils-Light Mode"
-      showAlert("Light Mode Enabled", "success")
-    }
-  }
+  };
   return (
-
     <>
-
-
       <Router>
-
-
-        <Navbar title="Text Utils" aboutText="About T" mode={mode} toggleMode={toggleMode}></Navbar>
+        <Navbar
+          title="Text Utils"
+          aboutText="About T"
+          xx={funn}
+          mode={mode}
+          toggleMode={toggleMode}
+        ></Navbar>
         <Alert alert={alert} />
         <div className="container my-3">
           <Routes>
-
-            <Route exact path="/about" element={<About />}>
-
-            </Route>
-            <Route exact path="/" element={<TextForm heading="Enter text" showAlert={showAlert} mode={mode}></TextForm>}>
-
-            </Route>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route
+              exact
+              path="/"
+              element={
+                <TextForm
+                  heading="Enter text"
+                  showAlert={showAlert}
+                  mode={mode}
+                ></TextForm>
+              }
+            ></Route>
           </Routes>
         </div>
       </Router>
-
-
     </>
   );
 }
