67490ed4d65e13e7ca87576b864212c28ded7033
Sivanshu Jaiswal <shivanshu5416jaiswal@gmail.com>
2026-07-04 00:09:16 +0530

    test changes

--- src/App.js
+++ src/App.js
@@ -70,6 +70,22 @@ function App() {
           mode={mode}
           toggleMode={toggleMode}
         ></Navbar>
+        <div style={{ width: "100%", height: "600px", position: "relative" }}>
+          <DotGrid
+            mouseRepulsion
+            mouseInteraction
+            density={1}
+            glowIntensity={0.3}
+            saturation={0}
+            hueShift={140}
+            twinkleIntensity={0.3}
+            rotationSpeed={0.1}
+            repulsionStrength={2}
+            autoCenterRepulsion={0}
+            starSpeed={0.5}
+            speed={1}
+          />
+        </div>
         <Alert alert={alert} />
         <div className="container my-3">
           <Routes>
